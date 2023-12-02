9855ac67c8e400882d7437699b0fa4b1c058cd0b
newrodrigo <rodrigocerqueira@outlook.com>
2023-12-02 15:32:44 -0300

    Comments component

---
+++ src/components/Comments/index.tsx
@@ -0,0 +1,45 @@
+import "./styles.css";
+
+export default function Comments() {
+  return (
+    <div className="comments-container">
+      <h2>O que estão dizendo</h2>
+      <div className="card-comment">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae harum
+          voluptatem ad fugit id distinctio.
+        </p>
+      </div>
+      <div className="card-comment">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae harum
+          voluptatem ad fugit id distinctio.
+        </p>
+      </div>
+      <div className="card-comment">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae harum
+          voluptatem ad fugit id distinctio.
+        </p>
+      </div>
+      <div className="card-comment">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae harum
+          voluptatem ad fugit id distinctio.
+        </p>
+      </div>
+      <div className="card-comment">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae harum
+          voluptatem ad fugit id distinctio.
+        </p>
+      </div>
+      <div className="card-comment">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae harum
+          voluptatem ad fugit id distinctio.
+        </p>
+      </div>
+    </div>
+  );
+}
