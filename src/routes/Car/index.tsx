841897b79ae1ad2f9ed126c2d53b5ed154822079
newrodrigo <rodrigocerqueira@outlook.com>
2023-12-02 14:39:07 -0300

    Created car card

--- src/routes/Car/index.tsx
+++ src/routes/Car/index.tsx
@@ -1,5 +1,23 @@
+import "./styles.css";
+import Card from "../../components/Card";
 import Header from "../../components/Header";
 
 export default function Car() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <main>
+        <section id="ct-car-section">
+          <div className="container">
+            <h2 className="ct-title">Venha nos visitar</h2>
+
+            <div className="card">
+              <Card />
+              <Card />
+            </div>
+          </div>
+        </section>
+      </main>
+    </>
+  );
 }
